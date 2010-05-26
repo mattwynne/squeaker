e677c2feb4ba409611592b0c270eddf4f897dd9c
Matt Wynne <matt@mattwynne.net>
2010-05-26 21:57:41 +0100

    Adding scenario for showing messages from followed users as well

--- features/step_definitions/model_steps.rb
+++ features/step_definitions/model_steps.rb
@@ -13,6 +13,11 @@ Given /^the User has posted a message with the content "([^\"]*)"$/ do |content|
   Message.create!(:content => content, :user_id => the_user.id)
 end
 
+Given /^the User "([^\"]*)" has posted a message with the content "([^\"]*)"$/ do |username, content|
+  user = User.find_by_username(username)
+  Message.create!(:content => content, :user_id => user.id)
+end
+
 Given /^there are 11 messages, each with different content$/ do
   11.times do |i|
     Message.create!(:content => "This is message number #{i + 1}")
