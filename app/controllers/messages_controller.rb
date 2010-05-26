3f3c6456d09c7f86ee9e2e2bfd9389318ba52daf
Matt Wynne <matt@mattwynne.net>
2010-05-26 14:41:09 +0100

    logging in

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,12 @@
+class MessagesController < ApplicationController
+  def index
+    @logged_in_user = logged_in_user
+  end
+  
+  private
+  
+  def logged_in_user
+    user_id = session[:logged_in_user_id]
+    User.find_by_id(user_id)
+  end
+end
