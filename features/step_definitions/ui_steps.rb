b37b53891dd67cadc1f8bce51cbd437de9f87d86
Giuseppe Alletti <alletti@mac.com>
2010-05-26 14:51:40 +0100

    messages controller is in

--- features/step_definitions/ui_steps.rb
+++ features/step_definitions/ui_steps.rb
@@ -3,5 +3,9 @@ Given /^I am logged in as the User$/ do
   visit user_sessions_path
   fill_in "Username", :with => user.username
   click_button "Log in"
-  assert page.has_content?("hello #{user.username}"), "Didn't find 'hello' stuff"
+  # assert page.has_content?("hellooo #{user.username}"), "Didn't find 'hello' stuff"
+  unless page.has_content?("hello #{user.username}")
+    save_and_open_page
+    raise "Ouch!!"
+  end
 end
