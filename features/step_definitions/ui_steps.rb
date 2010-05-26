d1626e61eeb2f2fd4eaab63d4b20e813f6dd9add
Matt Wynne <matt@mattwynne.net>
2010-05-26 14:50:33 +0100

    Use save and open page

--- features/step_definitions/ui_steps.rb
+++ features/step_definitions/ui_steps.rb
@@ -3,5 +3,8 @@ Given /^I am logged in as the User$/ do
   visit user_sessions_path
   fill_in "Username", :with => user.username
   click_button "Log in"
-  assert page.has_content?("hello #{user.username}")
+  unless page.has_content?("hello #{user.username}")
+    save_and_open_page
+    raise("Page didn't contain expected text")
+  end
 end
