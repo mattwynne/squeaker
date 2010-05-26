f8108599ce8c8d0b8a6fdfb603f1cb1957899acc
Matt Wynne <matt@mattwynne.net>
2010-05-26 21:11:55 +0100

    Failing test for homepage

--- features/step_definitions/model_steps.rb
+++ features/step_definitions/model_steps.rb
@@ -5,3 +5,9 @@ end
 Given /^the User has posted a message with the content "([^\"]*)"$/ do |content|
   Message.create!(:content => content, :user_id => the_user.id)
 end
+
+Given /^there are 11 messages, each with different content$/ do
+  11.times do |i|
+    Message.create!(:content => "This is message number #{i + 1}")
+  end
+end
--- features/step_definitions/ui_steps.rb
+++ features/step_definitions/ui_steps.rb
@@ -1,12 +1,20 @@
+module ContentAssertions
+  def assert_content(content)
+    unless page.has_content?(content)
+      save_and_open_page
+      raise("Expected content '#{content}' was not found.")
+    end
+  end
+end
+
+World(ContentAssertions)
+
 Given /^I am logged in as the User$/ do
   user = User.first
   visit user_sessions_path
   fill_in "Username", :with => user.username
   click_button "Log in"
-  unless page.has_content?("hello #{user.username}")
-    save_and_open_page
-    raise("Page didn't contain expected text")
-  end
+  assert_content("hello #{user.username}")
 end
 
 When /^I visit the homepage$/ do
@@ -14,8 +22,19 @@ When /^I visit the homepage$/ do
 end
 
 Then /^I should see "([^\"]*)" in my feed$/ do |expected_text|
-  unless page.has_content?(expected_text)
-    save_and_open_page
-    raise("I want to see #{expected_text}")
+  within('#messages') do
+    assert_content(expected_text)
   end
+end
+
+Then /^I should see the latest 10 messages$/ do
+  Message.all(:limit => 10, :order => "created_at DESC").each do |message|
+    within("#messages") do
+      assert_content(message.content)
+    end
+  end
+end
+
+Then /^I should see exactly (\d+) messages and no more$/ do |expected_number|
+  assert_equal(expected_number.to_i, all(:css, '#messages .message').length)
 end
