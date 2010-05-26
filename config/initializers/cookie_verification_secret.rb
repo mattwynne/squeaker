292789c8a3c72ce881c00c275f767ca5775761f5
Giuseppe Alletti <alletti@mac.com>
2010-05-26 14:25:09 +0100

    Missing greeting

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '4dbb72d8374c1a94b15528150a04bed753eb7575ba7e864afb253d8dd56994185c322a6ada6270c261c5d9df321ec9322ebd61f874988586f6298add473c7ee5';
