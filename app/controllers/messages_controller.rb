98e686552d74b5b33500700ec04fafcd842ebc6e
Giuseppe Alletti <alletti@mac.com>
2010-05-26 16:22:20 +0100

    better message list, added css

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,16 @@
 class MessagesController < ApplicationController
   def index
     @logged_in_user = logged_in_user
-    @message = Message.new
+    @message = Message.new :user_id => logged_in_user.id
+    @messages = logged_in_user.messages
   end
   
   def create
-    user_id = logged_in_user.id
-    content = params[:message][:content]
+    # user_id = logged_in_user.id
+    # content = params[:message][:content]
     
-    Message.create! :user_id => user_id, :content => content
+    # Message.create! :user_id => user_id, :content => content
+    Message.create! params[:message]
     
     redirect_to :action => :index
   end
