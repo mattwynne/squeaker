d5402fe8db1467c49c9c398946fa69137c5c48b3
Giuseppe Alletti <alletti@mac.com>
2010-05-27 14:08:33 +0100

    Fix Matt's silly mistake

--- features/support/user_helper.rb
+++ features/support/user_helper.rb
@@ -1,9 +1,7 @@
 module UserHelper
   def the_user
-    unless username
-      raise("There is more than one User!") unless User.count == 1
-      User.first
-    end
+    raise("There is more than one User!") unless User.count == 1
+    User.first
   end
 end
 
