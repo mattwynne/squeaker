db67b79ffa0e2ac93f55978da2544c62afa5aca2
Matt Wynne <matt@mattwynne.net>
2012-01-17 12:09:35 +0000

    Update URL

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,7 +4,7 @@ require 'test/unit/assertions'
 
 Capybara.default_driver = :selenium
 #Capybara.app_host = 'http://squeaker.heroku.com'
-Capybara.app_host = 'http://10.94.7.15:3000'
+Capybara.app_host = 'http://10.164.43.166:3000'
 
 World(Capybara::DSL)
 World(Test::Unit::Assertions)
