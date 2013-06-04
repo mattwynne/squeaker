b849a6cf9b074064e6732438cbb86a23b7781d41
Matt Wynne <matt@mattwynne.net>
2013-06-04 13:15:30 +0100

    Extract out UserSession model

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,21 +1,8 @@
-class UserSession
-  extend ActiveModel::Naming
-  include ActiveModel::Conversion
-  attr_accessor :username
-  
-  def id
-  end
-
-  def persisted?
-    false
-  end
-end
-
 class UserSessionsController < ApplicationController
   def new
     @user_session = UserSession.new
   end
-  
+
   def create
     username = params[:user_session][:username]
     user = User.find_by_username(username)
@@ -23,10 +10,10 @@ class UserSessionsController < ApplicationController
       raise("No user with username '#{username}'")
     end
     session[:logged_in_user_id] = user.id
-    
+
     redirect_to root_path, :status => 303
   end
-  
+
   def destroy
     session[:logged_in_user_id] = nil
     redirect_to root_path
